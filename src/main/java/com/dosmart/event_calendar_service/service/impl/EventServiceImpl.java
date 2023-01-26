1b76c885882ab8615248e81ba66a3a1a01552557
Hari-Nikesh-R <hari.nikesh.r.cce@gmail.com>
2023-01-26 20:51:50 +0530

    Completed DDL operations for event

--- src/main/java/com/dosmart/event_calendar_service/service/impl/EventServiceImpl.java
+++ src/main/java/com/dosmart/event_calendar_service/service/impl/EventServiceImpl.java
@@ -1,9 +1,12 @@
 package com.dosmart.event_calendar_service.service.impl;
 
+import com.dosmart.event_calendar_service.dtos.BaseResponse;
 import com.dosmart.event_calendar_service.model.CalendarEvent;
 import com.dosmart.event_calendar_service.repository.CalendarEventRepository;
 import com.dosmart.event_calendar_service.service.EventService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
@@ -28,6 +31,24 @@ public class EventServiceImpl implements EventService<CalendarEvent> {
         calendarEvent.setCreatedBy("hari.nikesh.r.cce@gmail.com");
         calendarEvent.setStatus("ACTIVE");
         return calendarEventRepository.save(calendarEvent);
+    }
+
+    @Override
+    public BaseResponse<String> modifyEvent(String eventId, CalendarEvent calendarEvent) {
+        Optional<CalendarEvent> optionalCalendarEvent =  calendarEventRepository.findById(eventId);
+        if(optionalCalendarEvent.isPresent())
+        {
+            BeanUtils.copyProperties(calendarEvent, optionalCalendarEvent.get());
+            calendarEventRepository.save(optionalCalendarEvent.get());
+            return new BaseResponse<>("Modification Successful", HttpStatus.OK.value(),true,"","Modified Event");
+        }
+        return new BaseResponse<>("Modification UnSuccessful", HttpStatus.NO_CONTENT.value(), false, "Event not found, Something went wrong",null);
+    }
+
+    @Override
+    public BaseResponse<String> deleteEvent(String eventId) {
+        calendarEventRepository.deleteById(eventId);
+        return new BaseResponse<>("Deleted Successfully",HttpStatus.OK.value(),true,"","Deleted the event");
     }
 
     @Override
